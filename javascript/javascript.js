b8d139c37f89a4468400921d2387ab33e3b51543
chiehan1 <chieh.an.hsu@gmail.com>
2015-10-19 17:42:14 +0800

    hello

---
+++ javascript/javascript.js
@@ -0,0 +1,31 @@
+console.log("Jack".length);
+
+//confirm('Is there a window jumping up?');html;跳出視窗有是非選項;'是' 返回 true;'否' 返回 false;
+
+//prompt('How are you?');html;跳出視窗可輸入字;
+
+var test_boolean = "I am coding like a champ".length > 10;
+console.log(test_boolean);
+
+if (false) {
+    console.log("Let's go down the first road!");
+} else {
+    console.log("hello")
+}
+
+if (1 > 2) {
+    console.log("The condition is true")
+} else {
+    console.log("The condition is false")
+}
+
+if (true) {
+    console.log("I finished my first course!");
+} else {
+    console.log("Not yet!");
+}
+
+console.log(11 % 2);//顯示餘數;
+
+var test_substring = "wonderful day".substring(3, 7);
+console.log(test_substring);
